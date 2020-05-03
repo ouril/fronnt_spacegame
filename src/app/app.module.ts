23328add7225a2461fc40b48663b6a46de9b5e0a
Andrey Gruzdev <andrey.gruzdev@sifox.ru>
2020-05-03 16:11:54 +0300

    added type

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { UnitComponentComponent } from './unit-component/unit-component.component';
+import { GamesComponentComponent } from './games-component/games-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnitComponentComponent,
+    GamesComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
